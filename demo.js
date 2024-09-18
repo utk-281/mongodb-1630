c6461921db262f7c144dfa1290fb85dc4f435623
utkarsh <utkarshgupta.281@gmail.com>
2024-09-18 17:51:24 +0530

    add intro to mongodb

---
+++ demo.js
@@ -0,0 +1,32 @@
+//! ARRAYS
+// let, const ,var = []
+
+let arr = ["a", "b", "c", true, 12.2, undefined, null, ["string", true]];
+// console.log(arr[7][1]);
+
+let arrOfObjects = [
+  {
+    name: "John",
+  },
+  { age: 12, class: "7" },
+  { email: "email" },
+];
+// console.log(arrOfObjects[1].class);
+
+//! objects
+let obj = {
+  name: "John",
+  age: 12,
+  class: "7",
+  email: "email",
+  age: 23,
+  0: "zero",
+  skills: {
+    frontend: ["html", "css", "js"],
+    backend: ["js", "java", "python"],
+  },
+};
+// console.log(obj.skills.backend[1]);
+
+// https://www.mongodb.com/try/download/community ==> mongodb compass
+// https://www.mongodb.com/try/download/shell ==> shell
